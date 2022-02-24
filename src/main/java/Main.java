a2df2ca58f8881d3dfb3dd1f2edc731b2e0c39bc
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-02-24 17:46:52 -0500

    created sum of input formula

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,32 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Main main_object = new Main();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        //System.out.println(main_object.sumOfNum(3));
+        //System.out.println(1+2+3);
+
+        int newN = scanner.nextInt();
+        int result = main_object.sumOfNum(newN);
+        System.out.println(result);
+        scanner.close();
+    }
+
+    int sumOfNum ( int n){
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+
+        }
 
     }
 }
